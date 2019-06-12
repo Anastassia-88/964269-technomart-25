9b9182fdfee4903c8d3d4e0c3692955b625e1b99
Anastassia-88 <anastassia.russak@gmail.com>
2019-06-12 07:23:03 +0200

    20. Основное задание: завершаем вёрстку личного проекта.

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var link = document.querySelector(".header-user-navigation-button-login");
+
+var popup = document.querySelector(".modal-login");
+
+link.addEventListener("click", function (evt)
+{
+    evt.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
